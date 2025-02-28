0a801f1e1c651105479abe79ae604ef08b53ecf7
Sriharshini43 <109490959+Sriharshini43@users.noreply.github.com>
2025-02-28 16:45:10 +0530

    Update AddBook.jsx

--- src/pages/AddBook.jsx
+++ src/pages/AddBook.jsx
@@ -15,7 +15,7 @@ function AddBook() {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:3000/api/books", book);
+      const response = await axios.post("https://lms-production-f51d.up.railway.app/api/books", book);
       if (response.data.success) {
         alert("Book added successfully");
         navigate("/bookscrud");
