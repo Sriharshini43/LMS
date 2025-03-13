91e82e034e60ebc707da4e4c856e4e040429b540
Sriharshini43 <109490959+Sriharshini43@users.noreply.github.com>
2025-03-13 11:54:56 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true,  // Ensures proper routing in dev mode
+  },
+  build: {
+    outDir: 'dist',  // Default output folder for Vercel
+  },
+});
