66b766faf42c95bd6caea9025398cbbdb149b754
Sriharshini43 <109490959+Sriharshini43@users.noreply.github.com>
2025-06-20 15:18:04 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -42,7 +42,7 @@ import React, { useState, useEffect } from "react";
      }
  
      try {
-       const response = await axios.post("https://lms-production-5bae.up.railway.app/api/auth/login", { email, password, userType });
+       const response = await axios.post("${process.env.BACKEND_URL}/api/auth/login", { email, password, userType });
  
        if (response.data.success) {
          toast.success("Login successful!");
